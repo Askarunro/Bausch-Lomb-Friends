c65a7cab86be80c1648fa3ad2118c975aa009d1f
Askarunro <paskar.y.a@gmail.com>
2021-10-31 00:23:11 +0300

    update

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $('.slider').slick({
-    arrows: true,
-    dots: false,
+    arrows: false,
+    dots: true,
     speed: 200,
     easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
     infinite: false,
