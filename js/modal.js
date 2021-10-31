d4a0b01c216332a913aa33cb5dea2304bf0f1a10
Askarunro <paskar.y.a@gmail.com>
2021-10-31 18:42:02 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,16 @@
-// (() => {
-//         const refs = {
-//             openModalBtn: document.querySelector('[data-modal-open]'),
-//             closeModalBtn: document.querySelector('[data-modal-close]'),
-//             modal: document.querySelector('[data-modal]'),
-//         };
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-//             refs.openModalBtn.addEventListener('click', toggleModal);
-//             refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn[0].addEventListener('click', toggleModal);
+  refs.openModalBtn[1].addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//         function toggleModal() {
-//             document.body.classList.toggle("modal-open");
-//             refs.modal.classList.toggle('backdrop--hidden');
-//         }
-// })();
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('backdrop--hidden');
+  }
+})();
